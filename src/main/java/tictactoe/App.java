978bf402ac1815aae11cbe3b123a29af27144f53
Ivan Prizov <ivanko.priz@gmail.com>
2021-05-25 21:08:24 +0300

    Init project

---
+++ src/main/java/tictactoe/App.java
@@ -0,0 +1,14 @@
+package tictactoe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        
+        System.out.println( "Hello World!" );
+    }
+}
