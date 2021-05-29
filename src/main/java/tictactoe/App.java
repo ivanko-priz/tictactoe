4e18e1fb8503ac7a6e0504b55afb965518e6f0a5
Ivan Prizov <ivanko.priz@gmail.com>
2021-05-29 11:37:57 +0300

    Add playWithAutomaton for simple demo

--- src/main/java/tictactoe/App.java
+++ src/main/java/tictactoe/App.java
@@ -1,14 +1,48 @@
 package tictactoe;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args ) throws Exception
     {
+        playWithAutomaton();
+    }
+
+    public static void playWithAutomaton() throws Exception {
         Game g = new Game();
         Automaton a = new Automaton(g);
+        Scanner in = new Scanner(System.in);
+        
+        while (!g.isGameOver()) {
+            System.out.print("Your turn, human: ");
+            int move = in.nextInt();
 
-        int goodMove = a.findGoodMove();
+            System.out.println("Human played " + move);
+            g.makeMove(move);
+            System.out.println(Arrays.toString(g.getField()));
 
-        System.out.println("Good move is: " + goodMove);
+            move = a.findGoodMove();
+            System.out.println("Machine played " + move);
+            g.makeMove(move);
+
+            System.out.println(Arrays.toString(g.getField()));
+        }
+
+        in.close();
+
+        System.out.println("Game over");
+        switch (g.getWinner()) {
+            case 0:
+                System.out.println("Draw");
+                break;
+            case 'x':
+                System.out.println("Human won");
+                break;
+            default:
+                System.out.println("Machine won");
+                break;
+        }
     }
 }
